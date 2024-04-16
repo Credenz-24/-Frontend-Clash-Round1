5952b4b0a04257b49804898e86aadc1fefa72cf8
Palak Agrawal <palaka2805@gmail.com>
2024-04-16 17:41:54 +0530

    Added Result UI

--- src/pages/ResultPg/shapes/Circles.jsx
+++ src/pages/ResultPg/shapes/Circles.jsx
@@ -7,29 +7,30 @@ const Circles = (props) => {
 
     const circleStyles = {
         "fill": "none",
-        "stroke": "#042AA7",
+        "stroke": "rgb(59 130 246)",
         "strokeWidth": "20px",
         "strokeDasharray": "400",
         "strokeDashoffset": sodf,
-        "animation": "anim 1s linear forwards"
+        "animation": "anim 1s linear forwards",
+        "backgroundColor":"red"
     }
 
     return ( 
         
         <>
-            <div className="skills h-[22vh] w-[22vh] relative  flex justify-center items-center ">
-                <div className='outercircle1 h-[22vh] w-[22vh] rounded-full border-2 border-[white] border-solid  flex justify-center items-center p-[20px] '>
-                    <div className='innercircle h-[16vh] w-[16vh] bg-slate-950 rounded-full border-2 border-[white] border-solid flex justify-center items-center'>
-                        <div className = "number font-[600] text-white ">
+            <div className="skills h-[22vh] w-[22vh] relative  justify-center items-center bg-green-900">
+                <div className='outercircle1 h-[22vh] w-[22vh] rounded-full border-2 border-[white] border-solid  flex justify-center items-center p-[20px]'>
+                    <div className='innercircle h-[16vh] w-[16vh] rounded-full border-2 border-[white] border-solid flex justify-center items-center'>
+                        <div className = "number font-[600] text-white">
                             {num}%    
                         </div>  
                     </div> 
                     
                     
                 </div>
-                <svg className=" top-[0%] left-[0%] absolute" xmlns ="http://www.w3.org/2000/svg"version="1.1" width="22vh" height ="22vh">
+                <svg className=" bg-red-500 opacity-[40%] h-full w-full flex justify-center items-center absolute " xmlns ="http://www.w3.org/2000/svg"version="1.1" width="22vh" height ="22vh">
 
-                    <circle style={circleStyles} className="" cx="80" cy="80" r="65" stroke-linecap="round"/>
+                    <circle style={circleStyles} className="" cx="77" cy="77" r="64" stroke-linecap="round"/>
 
                     </svg>
             </div>
